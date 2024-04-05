651a82f9726d5f9097eacf5ae0d667492537ec79
Sviat <slsvyatko@gmail.com>
2024-04-05 15:20:56 +0200

    added missed home controller test in order to achieve 100% coverage of controller package!!!

---
+++ src/test/java/ua/foxminded/backend/controller/home/HomeControllerTest.java
@@ -0,0 +1,24 @@
+package ua.foxminded.backend.controller.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(HomeController.class)
+class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testListEntities_shouldReturnViewOfEntity() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("components/home/home.html"));
+    }
+}
